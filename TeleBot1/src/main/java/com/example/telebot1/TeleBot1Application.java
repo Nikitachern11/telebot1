efe5088c476f24749c72bf03b53301c8b8e860c9
Nikita <melianas@student.21-school.ru>
2023-10-10 18:14:42 +0300

    answer to start message

---
+++ TeleBot1/src/main/java/com/example/telebot1/TeleBot1Application.java
@@ -0,0 +1,13 @@
+package com.example.telebot1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleBot1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeleBot1Application.class, args);
+	}
+
+}
---
+++ TeleBot1/src/test/java/com/example/telebot1/TeleBot1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.telebot1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleBot1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
