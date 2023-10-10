0d56648b3377d915dbbb80fe5fbca0740230be4c
Nikita <melianas@student.21-school.ru>
2023-10-10 21:13:53 +0300

    logback added

--- TeleBot1/src/main/java/com/example/telebot1/service/TelegramBot.java
+++ TeleBot1/src/main/java/com/example/telebot1/service/TelegramBot.java
@@ -1,12 +1,13 @@
 package com.example.telebot1.service;
 
 import com.example.telebot1.config.BotConfig;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-
+@Slf4j
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
 
@@ -39,6 +40,7 @@ public class TelegramBot extends TelegramLongPollingBot {
 
     private void startCommandReceived(long chatId, String firstName) throws TelegramApiException {
         String answer = "Hi, " + firstName + ", nice to meet you!";
+        log.info("Replied to user " + firstName);
         sendMessage(chatId, answer);
 
     }
@@ -51,7 +53,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         try  {
             execute(message);
         } catch (TelegramApiException e) {
-            throw new RuntimeException(e);
+            log.error("Error occured: " + e.getMessage());
         }
     }
 
